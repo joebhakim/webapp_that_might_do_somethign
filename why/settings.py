06e6b21683ee0a2314656d5821df755dedb8f574
joebhakim <joebhakim@gmail.com>
2020-07-17 18:00:29 -0400

    asfdoij

--- why/settings.py
+++ why/settings.py
@@ -119,4 +119,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/home/joe/django-apps/messageboard_app/messageboard/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
